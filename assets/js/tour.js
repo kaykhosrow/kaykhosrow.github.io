829413584d6894117dc03212da3e9f3f4dd10e73
Omar Kaykhusraw <101812026+kaykhosrow@users.noreply.github.com>
2026-05-17 17:38:09 +0100

    Update tour.js

--- assets/js/tour.js
+++ assets/js/tour.js
@@ -58,8 +58,8 @@
       arrowImg:      1,
       baseAngle:     0,
       arrowOffsetX:  15,
-      arrowOffsetY:  -35,
-      textOffsetY:   -5,
+      arrowOffsetY:  -60,
+      textOffsetY:   -15,
       textOffsetX:   15,
     },
     {
