b5fef000db92afef4c64624bc3191ec9db5ed9f1
Omar Kaykhusraw <101812026+kaykhosrow@users.noreply.github.com>
2026-05-18 03:18:07 +0100

    Update tour.js

--- assets/js/tour.js
+++ assets/js/tour.js
@@ -171,7 +171,7 @@
 
     stackEl = document.createElement('div');
     stackEl.id = 'tourStack';
-    stackEl.textContent = 'This site was built in HTML, CSS and JavaScript. The layout and styling are handled in CSS, whereas JavaScript takes care of all the animations, navigation and the interactions you see throughout the site. The source code is available in my GitHub repository.';
+    stackEl.textContent = 'This site was built in HTML, CSS and JavaScript. The layout and styling are handled in CSS, whereas JavaScript takes care of all the animations, navigation and interactions you experience throughout the site. The source code is available in my GitHub repository.';
     document.body.appendChild(stackEl);
 
     NOTES.forEach(function (note) {
