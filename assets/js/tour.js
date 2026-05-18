03152c59b9e403c4b51889e3892377242e59df09
Omar Kaykhusraw <101812026+kaykhosrow@users.noreply.github.com>
2026-05-18 12:37:09 +0100

    Update tour.js

--- assets/js/tour.js
+++ assets/js/tour.js
@@ -133,7 +133,7 @@
     '  width:' + STACK_W + 'px;',
     '  font-family:"Scribble",cursive;',
     '  font-size:1.5rem;',
-    '  line-height:1.9;',
+    '  line-height:1.875;',
     '  color:rgba(0,0,0,0.88);',
     '  text-align:justify;',
     '  text-align-last:left;',
@@ -247,7 +247,7 @@
     var sy  = window.scrollY;
     var sx  = window.scrollX;
 
-    var top  = rr.top + sy - stackEl.offsetHeight + 175;
+    var top  = rr.top + sy - stackEl.offsetHeight + 165;
     var left = cr.right + sx - 50;
 
     stackEl.style.top  = top  + 'px';
