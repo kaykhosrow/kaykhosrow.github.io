ac91f7da910056d2b184a68b722a40091f6a99c6
Omar Kaykhusraw <101812026+kaykhosrow@users.noreply.github.com>
2026-05-22 11:06:00 +0100

    Update tour.js

--- assets/js/tour.js
+++ assets/js/tour.js
@@ -94,7 +94,6 @@
     '}',
     '.tour-stack.t-on { opacity:1; }',
     '.tour-close-btn { pointer-events:auto !important; cursor:pointer !important; }',
-    '.tour-close-btn:hover { opacity:0.85 !important; }',
 
 
   ].join('\n');
@@ -125,7 +124,7 @@
     stackEl = document.createElement('div');
     stackEl.id        = 'tourStack';
     stackEl.className = 'tour-stack';
-    stackEl.innerHTML = '<span style="display:inline-block;font-size:1.5rem;margin-bottom:0.45em;opacity:0.75;letter-spacing:0.025em;text-transform:uppercase;border:3px solid rgba(0,0,0,0.625);border-radius:4px;padding:0px 9px;box-shadow:1px 1px 2.5px rgba(0,0,0,0.25);line-height:1.25;position:relative;top:-10px;left:-1px;">Page Overview</span> <span class="tour-close-btn" style="display:inline-block;font-size:1.5rem;margin-bottom:0.45em;opacity:0.75;letter-spacing:0.025em;text-transform:uppercase;border:3px solid rgba(0,0,0,0.625);border-radius:4px;padding:0px 9px;box-shadow:1px 1px 2.5px rgba(0,0,0,0.25);line-height:1.25;position:relative;top:-10px;left:140px;"><i class="ph-bold ph-arrow-u-up-left" style="font-size:0.9em;vertical-align:-0.1em;margin-right:8px;"></i>Return to Home</span><span style="display:block;line-height:1.5;position:relative;top:2.5px;">Welcome to my page. Here you can explore my research, teaching, and conference activities, as well as selected code, cinematography, and contact details for any enquiries or potential collaboration.</span>';
+    stackEl.innerHTML = '<span style="display:inline-block;font-size:1.5rem;margin-bottom:0.45em;opacity:0.75;letter-spacing:0.025em;text-transform:uppercase;border:3px solid rgba(0,0,0,0.625);border-radius:4px;padding:0px 9px;box-shadow:1px 1px 2.5px rgba(0,0,0,0.25);line-height:1.25;position:relative;top:-10px;left:-1px;">Page Overview</span> <span class="tour-close-btn" style="display:inline-block;font-size:1.5rem;margin-bottom:0.45em;opacity:0.75;letter-spacing:0.025em;text-transform:uppercase;border:3px solid rgba(0,0,0,0.625);border-radius:4px;padding:0px 9px;box-shadow:1px 1px 2.5px rgba(0,0,0,0.25);line-height:1.25;position:relative;top:-10px;left:145px;"><i class="ph-bold ph-arrow-u-up-left" style="font-size:0.9em;vertical-align:-0.1em;margin-right:7.5px;"></i>Return to Home</span><span style="display:block;line-height:1.5;position:relative;top:2.5px;">Welcome to my page. Here you can explore my research, teaching, and conference activities, as well as selected code, cinematography, and contact details for any enquiries or potential collaboration.</span>';
     document.body.appendChild(stackEl);
 
     stackEl2 = document.createElement('div');
@@ -138,7 +137,7 @@
     stackEl3.id        = 'tourStack3';
     stackEl3.className = 'tour-stack';
     stackEl3.style.width = STACK_W3 + 'px';
-    stackEl3.innerHTML = '<span style="display:inline-block;font-size:1.5rem;margin-bottom:0.45em;opacity:0.75;letter-spacing:0.025em;text-transform:uppercase;border:3px solid rgba(0,0,0,0.625);border-radius:4px;padding:0px 9px;box-shadow:1px 1px 2.5px rgba(0,0,0,0.25);line-height:1.25;position:relative;top:-10px;left:-1px;">Optimal Viewing</span> <span style="display:inline-block;font-size:1.5rem;margin-bottom:0.45em;opacity:0.75;letter-spacing:0.025em;text-transform:uppercase;border:3px solid rgba(0,0,0,0.625);border-radius:4px;padding:0px 9px;box-shadow:1px 1px 2.5px rgba(0,0,0,0.25);line-height:1.25;position:relative;top:-10px;left:62.5px;"><i class="ph ph-browser" style="font-size:0.9em;vertical-align:-0.1em;margin-right:5px;"></i>Chrome</span><span style="display:block;line-height:1.5;position:relative;top:2.5px;">Designed for desktop. For an optimal experience, view at full screen width. Some elements might not display as intended on narrower windows.</span>';
+    stackEl3.innerHTML = '<span style="display:inline-block;font-size:1.5rem;margin-bottom:0.45em;opacity:0.75;letter-spacing:0.025em;text-transform:uppercase;border:3px solid rgba(0,0,0,0.625);border-radius:4px;padding:0px 9px;box-shadow:1px 1px 2.5px rgba(0,0,0,0.25);line-height:1.25;position:relative;top:-10px;left:-1px;">Optimal Viewing</span> <span style="display:inline-block;font-size:1.5rem;margin-bottom:0.45em;opacity:0.75;letter-spacing:0.025em;text-transform:uppercase;border:3px solid rgba(0,0,0,0.625);border-radius:4px;padding:0px 9px;box-shadow:1px 1px 2.5px rgba(0,0,0,0.25);line-height:1.25;position:relative;top:-10px;left:62.5px;"><i class="ph ph-browser" style="font-size:0.9em;vertical-align:-0.1em;margin-right:7.5px;"></i>Chrome</span><span style="display:block;line-height:1.5;position:relative;top:2.5px;">Designed for desktop. For an optimal experience, view at full screen width. Some elements might not display as intended on narrower windows.</span>';
     document.body.appendChild(stackEl3);
 
     NOTES.forEach(function (note) {
