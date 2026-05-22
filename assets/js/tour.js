7da84f0e5fd6d8b00c3bc20d65b3da40be023f50
Omar Kaykhusraw <101812026+kaykhosrow@users.noreply.github.com>
2026-05-22 02:20:03 +0100

    Update tour.js

--- assets/js/tour.js
+++ assets/js/tour.js
@@ -116,7 +116,7 @@
     stackElC.id        = 'tourStackC';
     stackElC.className = 'tour-stack';
     stackElC.style.width = '230px';
-    stackElC.innerHTML = '<span style="display:block;line-height:1.5;opacity:0.75;font-size:1.9rem;">COPYRIGHT <i class="ph ph-copyright" style="font-size:0.85em;vertical-align:-0.1em;"></i> ' + new Date().getFullYear() + '</span>';
+    stackElC.innerHTML = '<span style="display:block;line-height:1.5;opacity:0.6;font-size:1.9rem;">COPYRIGHT <i class="ph ph-copyright" style="font-size:0.85em;vertical-align:-0.1em;"></i> ' + new Date().getFullYear() + '</span>';
     document.body.appendChild(stackElC);
 
     stackEl = document.createElement('div');
@@ -243,7 +243,7 @@
     var sx  = window.scrollX;
 
     var top  = rr.top + sy - stackEl.offsetHeight - 43.5;
-    var left = cr.right + sx + 105 - 230 - 7.5;
+    var left = cr.right + sx + 105 - 230 - 17.5;
 
     stackElC.style.top  = top  + 'px';
     stackElC.style.left = left + 'px';
