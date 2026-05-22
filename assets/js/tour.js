6ccc912af4bba64a81d89df35fb31add62c36d31
Omar Kaykhusraw <101812026+kaykhosrow@users.noreply.github.com>
2026-05-22 10:15:33 +0100

    Update tour.js

--- assets/js/tour.js
+++ assets/js/tour.js
@@ -123,7 +123,7 @@
     stackEl = document.createElement('div');
     stackEl.id        = 'tourStack';
     stackEl.className = 'tour-stack';
-    stackEl.innerHTML = '<span style="display:inline-block;font-size:1.5rem;margin-bottom:0.45em;opacity:0.75;letter-spacing:0.025em;text-transform:uppercase;border:3px solid rgba(0,0,0,0.625);border-radius:4px;padding:0px 9px;box-shadow:1px 1px 2.5px rgba(0,0,0,0.25);line-height:1.25;position:relative;top:-10px;left:-1px;">Page Overview</span><span style="display:block;line-height:1.5;position:relative;top:2.5px;">Welcome to my page. Here you can explore my research, teaching, and conference activity, as well as selected code, cinematography, and contact details for any enquiries or potential collaboration.</span>';
+    stackEl.innerHTML = '<span style="display:inline-block;font-size:1.5rem;margin-bottom:0.45em;opacity:0.75;letter-spacing:0.025em;text-transform:uppercase;border:3px solid rgba(0,0,0,0.625);border-radius:4px;padding:0px 9px;box-shadow:1px 1px 2.5px rgba(0,0,0,0.25);line-height:1.25;position:relative;top:-10px;left:-1px;">Page Overview</span><span style="display:block;line-height:1.5;position:relative;top:2.5px;">Welcome to my page. Here you can explore my research, teaching, and conference activities, as well as selected code, cinematography, and contact details for any enquiries or potential collaboration.</span>';
     document.body.appendChild(stackEl);
 
     stackEl2 = document.createElement('div');
