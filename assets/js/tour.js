9ff8fb1c51918d709991cdb56aeda3b99e5e03ce
Omar Kaykhusraw <101812026+kaykhosrow@users.noreply.github.com>
2026-05-22 03:16:21 +0100

    Update tour.js

--- assets/js/tour.js
+++ assets/js/tour.js
@@ -244,7 +244,7 @@
     var sx  = window.scrollX;
 
     var top  = rr.top + sy - stackEl.offsetHeight - 42;
-    var left = cr.right + sx + 105 - 230 - 7.5;
+    var left = cr.right + sx + 105 - 230 - 17.5;
 
     stackElC.style.top  = top  + 'px';
     stackElC.style.left = left + 'px';
